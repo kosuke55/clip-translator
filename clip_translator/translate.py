bb36d15682459811fa941ed1d8c6a2981cbfda55
kosuke55 <kosuke.tnp@gmail.com>
2020-11-13 08:58:15 +0900

    Format

--- clip_translator/translate.py
+++ clip_translator/translate.py
@@ -167,13 +167,10 @@ def kill_server():
     p = subprocess.Popen('lsof -i :{}'.format(PORT).split(),
                          stdout=subprocess.PIPE,
                          stderr=subprocess.STDOUT)
-
     pid = False
     for line in iter(p.stdout.readline, b''):
         if 'clip_t' in line:
             pid = line.split(' ')[1]
-            print(pid)
-
     if pid:
         cmd = 'kill {}'.format(pid)
         print(cmd)
